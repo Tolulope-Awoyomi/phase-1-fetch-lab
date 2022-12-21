eb137d0a467248337a109caeff96b6abad5f8de5
+ <+>
2022-12-20 22:19:47 -0500

    commit tree ba09528c192cc5ec499ba224cb9862f661165382

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (response) {
+    return response.json();
+  })
+  .then ( function (books) {
+    renderBooks(books)
+    console.log(books)
+  });
+ 
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +24,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
